450876130ed88753659fb1aaa7da98ed2eacd7a0
mouseH <donghao_37@163.com>
2020-02-07 23:40:00 +0800

    Create LeetCode_283_386.java

---
+++ Week_00/G20200343030386/LeetCode_283_386.java
@@ -0,0 +1,15 @@
+package com.example.task;
+
+public class LeetCode_283_386 {
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[index++] = num;
+            }
+        }
+        while (index < nums.length) {
+            nums[index++] = 0;
+        }
+    }
+}
