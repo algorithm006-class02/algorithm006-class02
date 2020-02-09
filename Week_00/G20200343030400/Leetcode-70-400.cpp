00df05ab335201ab12003aa4ecd6e04b5af90506
armeria-program <59239902+armeria-program@users.noreply.github.com>
2020-02-09 13:50:51 +0800

    Create Leetcode-70-400.cpp

---
+++ Week_00/G20200343030400/Leetcode-70-400.cpp
@@ -0,0 +1,14 @@
+//70-climbing-stairs
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> f(n+1);
+        f[0] = 1;
+        f[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            f[i] = f[i-1] + f[i-2];
+        }
+        return f[n];
+    }
+};
