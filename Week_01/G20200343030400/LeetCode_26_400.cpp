ec1217c4327a1dd9cc8ad3e306aa56fd183a9988
armeria-program <59239902+armeria-program@users.noreply.github.com>
2020-02-10 23:43:05 +0800

    Update LeetCode_26_400.cpp

--- Week_01/G20200343030400/LeetCode_26_400.cpp
+++ Week_01/G20200343030400/LeetCode_26_400.cpp
@@ -4,15 +4,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.size() == 0 )
-            return 0;
-        int uniques = 0;
-        for (int i = 1; i < nums.size(); ++i) {
-            if (nums[i] != nums[uniques]) {
-                ++uniques;
-                nums[uniques] = nums[i];
+        if (nums.size() == 0) return 0;
+        int unique = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != nums[unique]) {
+                ++unique;
+                nums[unique] = nums[i];
             }
         }
-        return (uniques + 1);
+        return (unique + 1);
     }
 };
