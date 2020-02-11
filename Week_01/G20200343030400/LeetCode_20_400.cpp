6ffd18e7a4d2f33381bca3eec12c69b2e6fe7f10
armeria-program <59239902+armeria-program@users.noreply.github.com>
2020-02-11 18:40:43 +0800

    Create LeetCode_20_400.cpp

---
+++ Week_01/G20200343030400/LeetCode_20_400.cpp
@@ -0,0 +1,29 @@
+//20
+//https://leetcode-cn.com/problems/valid-parentheses
+
+class Solution {
+public:
+    bool isValid(string s) {
+        int len = s.size();
+        if (len == 0) return true;
+        if (len % 2 == 1) return false; 
+        stack<char> stk;
+        for (int i = 0; i < len; ++i) {
+            if (stk.empty()) {
+                stk.push(s[i]);
+                continue;
+            } else if (s[i] == ')' && stk.top() == '(') {
+                stk.pop();
+                continue;
+            } else if (s[i] == ']' && stk.top() == '[') {
+                stk.pop();
+                continue;
+            } else if (s[i] == '}' && stk.top() == '{') {
+                stk.pop();
+                continue;
+            }
+            stk.push(s[i]);
+        }
+        return stk.empty();
+    }
+};
