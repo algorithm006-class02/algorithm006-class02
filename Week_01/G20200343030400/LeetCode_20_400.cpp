cad456fc85b519749d3228042a86ab8d3adc79a7
armeria-program <59239902+armeria-program@users.noreply.github.com>
2020-02-11 19:02:28 +0800

    Update LeetCode_20_400.cpp

--- Week_01/G20200343030400/LeetCode_20_400.cpp
+++ Week_01/G20200343030400/LeetCode_20_400.cpp
@@ -27,3 +27,24 @@ public:
         return stk.empty();
     }
 };
+
+class Solution-ascii {
+public:
+    bool isValid(string s) {
+        int len = s.size();
+        if (len == 0) return true;
+        if (len % 2 == 1) return false;
+        stack<char> stk;
+        for (int i = 0; i < len; ++i) {
+            if (stk.empty()) {
+                stk.push(s[i]);
+                continue;
+            } else if (s[i] - stk.top() <= 2 && s[i] - stk.top() >= 1) {   //ascii
+                stk.pop();
+                continue;
+            }
+            stk.push(s[i]);
+        }
+        return stk.empty();
+    }
+};
