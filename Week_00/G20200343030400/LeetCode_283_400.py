aa5524cda319c93863bc92449d77e60cf717cb41
armeria-program <59239902+armeria-program@users.noreply.github.com>
2020-02-12 16:21:59 +0800

    Update LeetCode_283_400.py

--- Week_00/G20200343030400/LeetCode_283_400.py
+++ Week_00/G20200343030400/LeetCode_283_400.py
@@ -2,9 +2,6 @@
 
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
         count_nonzeros = 0
         for i in range(len(nums)):
             if nums[i] != 0 :
