f217e27b02cc3a15ec40c6a3d615a1dc67e9b5e5
armeria-program <59239902+armeria-program@users.noreply.github.com>
2020-02-13 16:29:15 +0800

    Update LeetCode_66_400.cpp

--- Week_01/G20200343030400/LeetCode_66_400.cpp
+++ Week_01/G20200343030400/LeetCode_66_400.cpp
@@ -8,8 +8,8 @@ public:
         int carry = 1;
         for (int i = digits.size() - 1; i >= 0; --i) {
             sum = digits[i] + carry;
-            carry = sum / 10;
             digits[i] = sum % 10;
+            carry = sum / 10;
         }
         if (carry != 0) {
             digits.insert(digits.begin(), carry);
