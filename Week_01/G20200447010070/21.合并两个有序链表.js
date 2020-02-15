fc43ca0c08447f6c205fd7886c36e2177cf6f549
wendraw <zwd@tmdtouch.com>
2020-02-15 16:58:54 +0800

    Submmit questions 21 and 26

---
+++ Week_01/G20200447010070/21.合并两个有序链表.js
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=javascript
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+
+  let node = null;
+  if (l1.val < l2.val) {
+    node = l1;
+    l1 = l1.next;
+  } else {
+    node = l2;
+    l2 = l2.next;
+  }
+  let tail = node;
+  while (l1 != null && l2 != null) {
+    if (l1.val < l2.val) {
+      tail.next = l1;
+      l1 = l1.next;
+    } else {
+      tail.next = l2;
+      l2 = l2.next;
+    }
+    tail = tail.next;
+  }
+  tail.next = l1 === null ? l2 : l1;
+  return node;
+};
+// @lc code=end
