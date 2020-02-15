3a002c43ac09fb339fb710fc9f41424fb1535c59
gdhu <guodong_hu@126.com>
2020-02-15 15:23:49 +0800

    update readme

---
+++ Week_00/G20200343030364/LeetCode_1_364.java
@@ -0,0 +1,10 @@
+/**
+ * Created by HuGuodong on 2/7/20.
+ */
+public class LeetCode_1_364 {
+
+  public static void main(String[] args) {
+    System.out.println(1);
+    System.out.println(1);
+  }
+}
