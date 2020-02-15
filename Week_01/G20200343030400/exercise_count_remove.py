975574cd9c5df777d46966b10b12e77faa3c446f
armeria-program <59239902+armeria-program@users.noreply.github.com>
2020-02-15 16:27:57 +0800

    Update exercise_count_remove.py

--- Week_01/G20200343030400/exercise_count_remove.py
+++ Week_01/G20200343030400/exercise_count_remove.py
@@ -4,8 +4,7 @@ def remove(m, total = 100):
 	if m <= 1 or m >=100:
 		print("ERROR")
 		return []
-	
-	ans = []
+
 	circle = [0 for i in range(total)]
 	count = total
 	i = - 1
@@ -17,8 +16,5 @@ def remove(m, total = 100):
 		j += 1
 		if j % m == 0:
 			circle[i] = 1
-			count -= 1	
-	for i in range(total):
-		if circle[i] == 0:
-			ans.append(i + 1)	
-	return ans
+			count -= 1
+	return [idx + 1 for idx, a in enumerate(circle) if a == 0]
