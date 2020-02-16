02bf174735647d6f23ab4064563404a9b423fce7
limengyang <292927199@qq.com>
2020-02-16 10:14:07 +0800

    26题

---
+++ Week_01/G20200343030488/26.删除排序数组中的重复项.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=26 lang=python
+#
+# [26] 删除排序数组中的重复项
+#
+
+# @lc code=start
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+# @lc code=end
+
