d7ed67cc02c2c4ca045fa1102f5c9ed58a5ad0ea
Ping5626 <17610575626@163.com>
2020-02-16 16:35:18 +0800

    twosum finish

--- Week_01/G20200343030426/TwoSum1.java
+++ Week_01/G20200343030426/TwoSum1.java
@@ -1,7 +1,6 @@
 package com.ping.leetcode.easy;
 
 import java.util.*;
-import java.util.stream.IntStream;
 
 /**
  * @Author: 高一平
@@ -13,54 +12,69 @@ import java.util.stream.IntStream;
  * 给定 nums = [2, 7, 11, 15], target = 9
  * 因为 nums[0] + nums[1] = 2 + 7 = 9
  * 所以返回 [0, 1]
+ * <p>
+ * 分析
+ * 看了题解我很疑惑，没有考虑有多组题解存在的可能
+ * 我未注意不能重复利用这个数组中同样的元素，代码中没有对此做相关操作，依然被通过了...
+ * <p>
+ * 解惑
+ * 只需要返回一组结果即可
  **/
 public class TwoSum1 {
 
     /**
-     * 此代码已通过，但有一点需要注意
-     * 我未注意不能重复利用这个数组中同样的元素，代码中没有对此做相关操作，依然被通过了...
-     *
      * @param nums
      * @param target
      * @return
      */
     public int[] twoSum1(int[] nums, int target) {
-        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
-                    list.add(i);
-                    list.add(j);
+                    return new int[]{i, j};
                 }
             }
         }
-
-        int[] result = new int[list.size()];
-        IntStream.range(0, result.length).forEach(i -> result[i] = list.get(i));
-        return result;
+        return new int[]{};
     }
 
     /**
-     * 考虑不能重复利用这个数组中同样的元素
-     *
      * @param nums
      * @param target
      * @return
      */
     public int[] twoSum2(int[] nums, int target) {
-        Set<Integer> set = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    set.add(i);
-                    set.add(j);
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            Integer index = map.get(target - nums[i]);
+            if (index != null) {
+                if (i != index) {
+                    return new int[]{i, index};
                 }
             }
         }
+        return new int[]{};
+    }
 
-        int[] result = new int[set.size()];
-        Iterator<Integer> iterator = set.iterator();
-        IntStream.range(0, result.length).forEach(i -> result[i] = iterator.next());
-        return result;
+    /**
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum3(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            Integer index = map.get(target - nums[i]);
+            if (index != null) {
+                if (i != index) {
+                    return new int[]{i, index};
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
     }
 }
