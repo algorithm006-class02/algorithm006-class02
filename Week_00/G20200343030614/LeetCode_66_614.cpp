9ee95e8d1def2f6abb8c51731b61add0645c92eb
xiaozhoulove <45662455+xiaozhoulove@users.noreply.github.com>
2020-02-16 22:47:36 +0800

    614-Week 01

--- Week_00/G20200343030614/LeetCode_66_614.cpp
+++ Week_00/G20200343030614/LeetCode_66_614.cpp
@@ -1,3 +1,4 @@
+//两个解法思路一样，第二个利用不同的运算符，代码更简洁
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
@@ -8,6 +9,19 @@ public:
             } else {
                 return digits;
             }
+        }
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i=digits.size()-1; i>=0; i--) {
+            if (++digits[i]%=10)
+                return digits;
         }
         digits.insert(digits.begin(),1);
         return digits;
--- Week_00/G20200343030614/LeetCode_88_614.cpp
+++ Week_00/G20200343030614/LeetCode_88_614.cpp
@@ -1,3 +1,4 @@
+//用nums1创建一个临时数组，和nums2一个个元素比较，有序插入到nums1
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
@@ -14,5 +15,17 @@ public:
             nums1[k++] = nums2[j];
             j++;
         }
+    }
+};
+
+//把num2的元素补到num1后面，再全数组sort一下
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        nums1.resize(m);
+        for (int i=0; i<nums2.size(); i++) {
+            nums1.push_back(nums2[i]);
+        }
+        sort(nums1.begin(),nums1.end());
     }
 };
