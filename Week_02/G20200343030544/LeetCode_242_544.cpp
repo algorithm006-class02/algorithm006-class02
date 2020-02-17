5edb9426a3ca87bb0655f336f691e3ab2bfb94a8
zilong.xing <xingzilong2016@gmail.com>
2020-02-18 06:16:16 +0800

    242暴力法

--- Week_02/G20200343030544/LeetCode_242_544.cpp
+++ Week_02/G20200343030544/LeetCode_242_544.cpp
@@ -30,3 +30,13 @@ public:
 		else return false;
 	}
 };
+//暴力法
+class Solution {
+public:
+	bool isAnagram(std::string s, std::string t) {
+		if (s.length() != t.length()) return false;
+		std::sort(s.begin(),s.end());
+		std::sort(t.begin(), t.end());
+		return s == t;
+	}
+};
