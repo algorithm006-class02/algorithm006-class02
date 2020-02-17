222d49f0ed95e063f8714e23514461ec9077c54f
xiaozhoulove <45662455+xiaozhoulove@users.noreply.github.com>
2020-02-17 21:50:12 +0800

    614-Week 01

--- Week_01/G20200343030614/LeetCode_1_614.cpp
+++ Week_01/G20200343030614/LeetCode_1_614.cpp
@@ -13,10 +13,8 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int,int> a;//提供一对一的hash
         vector<int> b(2,-1);//用来承载结果，初始化一个大小为2，值为-1的容器b
-        for(int i=0;i<nums.size();i++)
-        {
-            if(a.count(target-nums[i])>0)
-            {
+        for(int i=0;i<nums.size();i++) {
+            if(a.count(target-nums[i])>0) {
                 b[0]=a[target-nums[i]];
                 b[1]=i;
                 break;
