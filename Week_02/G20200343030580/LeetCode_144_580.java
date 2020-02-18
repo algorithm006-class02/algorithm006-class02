007f805bebee134911a9a8ae1e04867d4621083b
jiangwenguang <hijwg@126.com>
2020-02-18 16:52:44 +0800

    144. 二叉树的前序遍历 非递归算法

--- Week_02/G20200343030580/LeetCode_144_580.java
+++ Week_02/G20200343030580/LeetCode_144_580.java
@@ -1,17 +1,26 @@
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        preorderTraversal(root, result);
-        return result;
-    }
-
-    private void preorderTraversal(TreeNode root, List<Integer> list) {
-        if (root == null) {
-            return;
-        } else {
-            list.add(root.val);
-            preorderTraversal(root.left, list);
-            preorderTraversal(root.right, list);
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode currentNode = root;
+        while (currentNode != null) {
+            result.add(currentNode.val);
+            if (currentNode.left != null) {
+                stack.push(currentNode);
+                currentNode = currentNode.left;
+            } else if (currentNode.right != null) {
+                currentNode = currentNode.right;
+            } else {
+                while (stack.isEmpty() == false && stack.peek().right == null) {
+                    stack.pop();
+                }
+                if (stack.isEmpty() == false) {
+                    currentNode = stack.pop().right;
+                } else {
+                    currentNode = null;
+                }
+            }
         }
+        return result;
     }
 }
