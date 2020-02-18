998a41ad32b59effd821b2c777bb6394916c2263
jiangwenguang <hijwg@126.com>
2020-02-18 21:41:20 +0800

    144题的优化版。 优化的关键点在于每个节点的子节点入栈的时候，先入右节点，再如左节点。这样遍历的时候就会先取到左节点。不用再通过"左节点"的父节点找对应的右节点了。

--- Week_02/G20200343030580/LeetCode_144_580.java
+++ Week_02/G20200343030580/LeetCode_144_580.java
@@ -1,24 +1,19 @@
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
         Stack<TreeNode> stack = new Stack<>();
-        TreeNode currentNode = root;
-        while (currentNode != null) {
-            result.add(currentNode.val);
-            if (currentNode.left != null) {
-                stack.push(currentNode);
-                currentNode = currentNode.left;
-            } else if (currentNode.right != null) {
-                currentNode = currentNode.right;
-            } else {
-                while (stack.isEmpty() == false && stack.peek().right == null) {
-                    stack.pop();
-                }
-                if (stack.isEmpty() == false) {
-                    currentNode = stack.pop().right;
-                } else {
-                    currentNode = null;
-                }
+        stack.push(root);
+        while (stack.isEmpty() == false) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
             }
         }
         return result;
