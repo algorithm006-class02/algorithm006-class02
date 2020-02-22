46faaaf6f04b38f14906f790b908e45bdeb40ab5
mouseH <donghao_37@163.com>
2020-02-23 01:42:35 +0800

    算法习题+自我总结

---
+++ Week_02/G20200343030386/LeetCode_49_386.java
@@ -0,0 +1,21 @@
+package com.example.leetCode.week2;
+
+import java.util.*;
+
+public class LeetCode_49_386 {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+}
