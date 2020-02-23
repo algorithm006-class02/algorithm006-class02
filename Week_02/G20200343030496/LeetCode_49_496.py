723fddd1aa8d8238bbdc35575ddb047db1ce5a01
louis <1530426574@qq.com>
2020-02-23 19:03:15 +0800

    496-Week 02

---
+++ Week_02/G20200343030496/LeetCode_49_496.py
@@ -0,0 +1,10 @@
+
+import collections
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return ans.values()
+
