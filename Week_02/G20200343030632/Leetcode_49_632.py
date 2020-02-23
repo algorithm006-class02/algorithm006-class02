ada63eb7b95b26501ef4141be927de4cf4bf4112
pastqing <116676671@qq.com>
2020-02-23 21:31:18 +0800

    add leetcode

---
+++ Week_02/G20200343030632/Leetcode_49_632.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=49 lang=python3
+#
+# [49] 字母异位词分组
+#
+
+# @lc code=start
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        return self.groupAnagrams_v1(strs)
+    def groupAnagrams_v1(self, strs: List[str]) -> List[List[str]]:
+        helper = {}
+        res = []
+        for item in strs:
+            key = ''.join(sorted(item))
+            if key in helper:
+                helper[key].append(item)
+            else:
+                helper[key] = [item]
+        
+        for val in helper.values():
+            res.append(val)
+
+        return res
+
+        
+# @lc code=end
+
---
+++ Week_02/G20200343030632/Leetcode_70_632.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode.cn id=70 lang=python3
+#
+# [70] 爬楼梯
+#
+
+# @lc code=start
+class Solution:
+    memory = {}
+    def climbStairs(self, n: int) -> int:
+        """
+            递归解决:
+            归纳条件: f(n) = f(n - 1) + f(n - 2)
+            1. 退出条件: n <= 2 return n
+            2. 
+        """
+        if n <= 2:
+            return n
+        if n - 2 in Solution.memory:
+            res1 = Solution.memory.get(n - 2)
+        else:
+            res1 = self.climbStairs(n - 2)
+            Solution.memory[n - 2] = res1
+        if n - 1 in Solution.memory:
+            res2 = Solution.memory.get(n - 1)
+        else:
+            res2 = self.climbStairs(n - 1)
+            Solution.memory[n - 1] = res2
+        return res1 + res2
+# @lc code=end
+
