e791e89b8d980f1e77ff3c3130efc319a49cd53a
armeria-program <59239902+armeria-program@users.noreply.github.com>
2020-02-24 10:23:32 +0800

    Update LeetCode_49_400.cpp

--- Week_02/G20200343030400/LeetCode_49_400.cpp
+++ Week_02/G20200343030400/LeetCode_49_400.cpp
@@ -1,23 +1,18 @@
-//40
+//49
 //https://leetcode-cn.com/problems/group-anagrams
 
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> res;
-        unordered_map<string, int> dict;
-        int num = 0;
-        string temp;
-        for (string str : strs) {
-            temp = str;
+        unordered_map<string, vector<string>> dict;
+        for (string s : strs) {
+            string temp = s;
             sort(temp.begin(), temp.end());
-            if (dict.count(temp)) {
-                res[dict[temp]].push_back(str);
-            } else {
-                vector<string> group(1, str);
-                res.push_back(group);
-                dict[temp] = num++;
-            }
+            dict[temp].push_back(s);
+        }
+        for (auto entry : dict) {
+            res.push_back(entry.second);
         }
         return res;
     }
