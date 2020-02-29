ddfdbd0d9348b3720aeed46bb996545d6b46e1cd
汪 琼 <joan@192.168.0.100>
2020-02-29 21:46:36 +0800

    提交作业

--- Week_03/G20200343030636/LeetCode_874_636.cpp
+++ Week_03/G20200343030636/LeetCode_874_636.cpp
@@ -142,6 +142,32 @@ public:
 
         return path ;
     }
+
+
+    /*
+     *社区高赞解法
+     */
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        unordered_set<string> obs;
+        for(int i=0;i<obstacles.size();i++) obs.insert(to_string(obstacles[i][0])+"#"+to_string(obstacles[i][1]));
+        int res=0, dir=0, x=0, y=0;
+        vector<vector<int>> ds={{0,1}, {1,0}, {0,-1}, {-1,0}};
+        for(int i=0;i<commands.size();i++) {
+            if(commands[i]==-2) dir--;
+            else if(commands[i]==-1) dir++;
+            else {
+                for(int j=0;j<commands[i];j++) {
+                    string pos=to_string(x+ds[dir][0])+"#"+to_string(y+ds[dir][1]);
+                    if(obs.find(pos)!=obs.end()) break;
+                    x+=ds[dir][0], y+=ds[dir][1];
+                }
+                res=max(res, x*x+y*y);
+            }
+            if(dir==-1) dir=3;
+            if(dir==4) dir=0;
+        }
+        return res;
+    }
 };
 // @lc code=end
 
