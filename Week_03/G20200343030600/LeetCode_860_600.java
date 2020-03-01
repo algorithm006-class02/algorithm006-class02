dc1443cdaedd242308e485e723334ea4557b8a7d
shi yang <bjyddx0625@gmail.com>
2020-03-01 21:36:50 +0800

    第三周作业和总结

---
+++ Week_03/G20200343030600/LeetCode_860_600.java
@@ -0,0 +1,26 @@
+/**
+ * 860. 柠檬水找零（贪心法）
+ */
+
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for(int b : bills){
+            if(b == 5) five ++;
+            else if(b == 10) {
+                if(five-- == 0) return false;
+                ten++;
+            }
+            else {
+                if(five > 0 && ten > 0){
+                    five--; ten--;
+                }
+                else if(five > 2){
+                    five -= 3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+}
