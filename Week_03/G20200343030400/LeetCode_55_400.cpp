808b6cf3211a618d453235b1e6ee74d5be52f64f
armeria-program <59239902+armeria-program@users.noreply.github.com>
2020-03-01 17:49:16 +0800

    Update LeetCode_55_400.cpp

--- Week_03/G20200343030400/LeetCode_55_400.cpp
+++ Week_03/G20200343030400/LeetCode_55_400.cpp
@@ -5,16 +5,14 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int numsSize = nums.size();
-        if (0 == numsSize) return false;
-        if (1 == numsSize) return true;
         int curRange = 0;
-        for (int i = 0; i < numsSize - 1; i++) {
+        for (int i = 0; i < numsSize; i++) {
             curRange = max(curRange - 1, nums[i]);
-            if (curRange == 0)
-                return false;
-            if (curRange >= numsSize - 1)
+            if (curRange >= numsSize - 1 - i)
                 return true;
+            if (curRange == 0)
+                return false;    
         }
-        return (curRange > 0);
+        return false;
     }
 }; 
