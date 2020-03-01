d1c8663de12db4f6254f8799db7f0f5ade12923a
armeria-program <59239902+armeria-program@users.noreply.github.com>
2020-03-02 00:04:01 +0800

    Update LeetCode_153_400.cpp

--- Week_03/G20200343030400/LeetCode_153_400.cpp
+++ Week_03/G20200343030400/LeetCode_153_400.cpp
@@ -7,17 +7,14 @@ public:
         int left = 0;
         int right = nums.size() - 1;
         int mid;
-        while (left <= right) {
+        while (left < right) {
             mid = left + (right - left) / 2;
-            if (nums[left] <= nums[mid] && nums[mid] <= nums[right]) {
-                return nums[left];
-            } else if (nums[left] <= nums[mid] && nums[mid] > nums[right]) {
+            if (nums[mid] > nums[right]) {
                 left = mid + 1;
             } else {
-                left++;
                 right = mid;
             }
         }
-        return nums[right];
+        return nums[left];
     }
 };
