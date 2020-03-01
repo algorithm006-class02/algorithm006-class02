1a4a6ed1617ed0496b35087781029079dea823cd
armeria-program <59239902+armeria-program@users.noreply.github.com>
2020-03-01 17:29:33 +0800

    Create LeetCode_55_400.cpp

---
+++ Week_03/G20200343030400/LeetCode_55_400.cpp
@@ -0,0 +1,18 @@
+//55
+//https://leetcode-cn.com/problems/jump-game
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int numsSize = nums.size();
+        if (0 == numsSize) return false;
+        if (1 == numsSize) return true;
+        int curRange = 0;
+        for (int i = 0; i < numsSize - 1; i++) {
+            curRange = max(curRange - 1, nums[i]);
+            if (curRange == 0)
+                return false;
+        }
+        return (curRange > 0);
+    }
+}; 
