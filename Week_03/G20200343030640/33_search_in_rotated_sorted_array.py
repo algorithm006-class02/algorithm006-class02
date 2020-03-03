3624a0dccf081a174ea889ee15031e5a819a8574
fuyadong <fuyadong2011@163.com>
2020-03-03 16:31:25 +0800

    修改33题算法逻辑

--- Week_03/G20200343030640/33_search_in_rotated_sorted_array.py
+++ Week_03/G20200343030640/33_search_in_rotated_sorted_array.py
@@ -1,24 +1,33 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if len(nums) == 0:
+        if not nums:
             return -1
-
+        
         low, hight = 0, len(nums) - 1
-        while low < hight:
-            mid = int((low + hight) / 2)
+        
+        while low <= hight:
+            mid = int(low + (hight - low) / 2)
+
             if target == nums[mid]:
                 return mid
-           
-            if ((nums[0] > target) and 
-            (nums[0] > nums[mid]) and 
-            (target > nums[mid])):
-                lo = mid + 1;
+            
+            if nums[low] <= nums[mid]:
+                # [low, mid] 有序
+                if target >= nums[low] and target < nums[mid]:
+                    # 向前规约
+                    hight = mid - 1
+                else:
+                    # 向后规约
+                    low = mid + 1
             else:
-                hi = mid;
-        
-        if lo == hi and nums[lo] == target:
-            return lo
-        else:
+                # [low, mid]无序，有拐点
+                if target > nums[mid] and target < nums[low]:
+                    # 向后规约
+                    low = mid + 1
+                else:
+                    # 向前规约
+                    hight = mid - 1
+        if low > hight:
             return -1
     
 
