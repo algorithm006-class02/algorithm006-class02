27c6ca4fbbeb5a7ad4062992f33f9463a7570875
luoqi <luoqi@bytedance.com>
2020-03-15 21:12:24 +0800

    590第五周作业

---
+++ Week_05/G20200343030590/LeetCode_647_590.java
@@ -0,0 +1,16 @@
+public class LeetCode_647_590 {
+
+    public int countSubstrings(String S) {
+        int N = S.length(), ans = 0;
+        for (int center = 0; center <= 2*N-1; ++center) {
+            int left = center / 2;
+            int right = left + center % 2;
+            while (left >= 0 && right < N && S.charAt(left) == S.charAt(right)) {
+                ans++;
+                left--;
+                right++;
+            }
+        }
+        return ans;
+    }
+}
