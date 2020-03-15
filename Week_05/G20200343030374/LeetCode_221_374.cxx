0bbe9cd95d6735fd8ed5c036c94e6966dcde122d
chenxianlong <794465731@qq.com>
2020-03-16 01:08:04 +0800

    374-Week 05

---
+++ Week_05/G20200343030374/LeetCode_221_374.cxx
@@ -0,0 +1,52 @@
+// https://leetcode-cn.com/problems/maximal-square/
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution_1 {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.empty()) {
+            return 0;
+        }
+        int m = matrix.size(), n = matrix[0].size(), sz = 0;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!i || !j || matrix[i][j] == '0') {
+                    dp[i][j] = matrix[i][j] - '0';
+                } else {
+                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+                sz = max(dp[i][j], sz);
+            }
+        }
+        return sz * sz;
+    }
+};
+
+class Solution_2 {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.empty()) {
+            return 0;
+        }
+        int m = matrix.size(), n = matrix[0].size(), sz = 0;
+        vector<int> pre(n, 0), cur(n, 0);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!i || !j || matrix[i][j] == '0') {
+                    cur[j] = matrix[i][j] - '0';
+                } else {
+                    cur[j] = min(pre[j - 1], min(pre[j], cur[j - 1])) + 1;
+                }
+                sz = max(cur[j], sz);
+            }
+            fill(pre.begin(), pre.end(), 0);
+            swap(pre, cur);
+        }
+        return sz * sz;
+    }
+};
