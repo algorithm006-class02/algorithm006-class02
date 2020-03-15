22cadcc3c78f5ebadc1b9a45b7269fc427cc54fe
armeria-program <59239902+armeria-program@users.noreply.github.com>
2020-03-15 16:31:21 +0800

    Update alternateSteps.cpp

--- Week_05/G20200343030400/alternateSteps.cpp
+++ Week_05/G20200343030400/alternateSteps.cpp
@@ -11,39 +11,39 @@ using namespace std;
 	总走法数就是dp[1][n] + dp[2][n] + dp[3][n]。
 */
 
-int alternateSteps(const int& n, const int& maxStep) {
-    vector<vector<int>> dp(maxStep + 1, vector<int>(max(n, maxStep) + 1, 0));
+int alternateSteps(const int& totalStairs, const int& maxStep) {
+    vector<vector<int>> dp(maxStep + 1, vector<int>(max(totalStairs, maxStep) + 1, 0));
     
     // 当总步数小于最大步长时，逐步增大步长范围
 	
-    for (int step = 1; step <= maxStep; step++) {
-        for (int i = 1; i < step; i++) {
-            dp[i][step] = dp[0][step - i] - dp[i][step - i];
-            dp[0][step] += dp[i][step];
+    for (int stairs = 1; stairs <= maxStep; stairs++) {
+        for (int step = 1; step < stairs; step++) {
+            dp[step][stairs] = dp[0][stairs - step] - dp[step][stairs - step];
+            dp[0][stairs] += dp[step][stairs];
         }
-        dp[step][step] = 1;
-        dp[0][step]++;
+        dp[stairs][stairs] = 1;
+        dp[0][stairs]++;
     }
 
     // 用上所有可能的步长
 	
-    for (int step = maxStep + 1; step <= n; step++) {
-        for (int i = 1; i <= maxStep; i++) {
-            dp[i][step] = dp[0][step - i] - dp[i][step - i];
-            dp[0][step] += dp[i][step];
+    for (int stairs = maxStep + 1; stairs <= totalStairs; stairs++) {
+        for (int step = 1; step <= maxStep; step++) {
+            dp[step][stairs] = dp[0][stairs - step] - dp[step][stairs - step];
+            dp[0][stairs] += dp[step][stairs];
         }
     }
 
-    return dp[0][n];
+    return dp[0][totalStairs];
 }
 
 int main()
 {
     int maxStep = 3;
     int n = 6;
-	for (int stairs = 0; stairs <= n; stairs++) {
-        cout << "n = " << stairs << "  f_n = " << alternateSteps(stairs, maxStep) << endl;
+    for (int totalStairs = 0; totalStairs <= n; totalStairs++) {
+        cout << "n = " << totalStairs << "  f_n = " << alternateSteps(totalStairs, maxStep) << endl;
     }
 	
-	return 0;
+    return 0;
 }
