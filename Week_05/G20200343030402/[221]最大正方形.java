4066a0549f7e27e336346614ff1642467167e5b1
ranpengcheng <ranpengcheng@yzw.cn>
2020-03-15 23:35:17 +0800

    402-Week 05

---
+++ Week_05/G20200343030402/[221]最大正方形.java
@@ -0,0 +1,48 @@
+//在一个由 0 和 1 组成的二维矩阵内，找到只包含 1 的最大正方形，并返回其面积。 
+//
+// 示例: 
+//
+// 输入: 
+//
+//1 0 1 0 0
+//1 0 1 1 1
+//1 1 1 1 1
+//1 0 0 1 0
+//
+//输出: 4 
+// Related Topics 动态规划
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[][] dp = new int[m][n];
+
+        int max = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = matrix[i][j] - '0';
+                    max = Math.max(max, dp[i][j]);
+                    continue;
+                }
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+
+        return max * max;
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
