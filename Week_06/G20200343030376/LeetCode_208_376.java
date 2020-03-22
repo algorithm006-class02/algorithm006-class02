18673810fa0a0669d95eb173444fcaaeb1d8edd9
朔 <virgo@virgos-MacBook-Pro.local>
2020-03-22 22:51:35 +0800

    376-Week 06

---
+++ Week_06/G20200343030376/LeetCode_208_376.java
@@ -0,0 +1,63 @@
+package com.example.lib;
+
+/**
+ * 所属项目:JavaProject
+ * 所属类名:com.example.lib.LeetCode_208_376
+ * 功能描述:
+ * 创建作者:VVs
+ * 创建时间:2020/3/22
+ */
+
+public class LeetCode_208_376 {
+
+
+    private TrieNode root;
+    public LeetCode_208_376() {
+        root = new TrieNode();
+        root.val = ' ';
+    }
+
+    public void insert(String word) {
+        TrieNode ws = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(ws.children[c - 'a'] == null){
+                ws.children[c - 'a'] = new TrieNode(c);
+            }
+            ws = ws.children[c - 'a'];
+        }
+        ws.isWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode ws = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            if(ws.children[c - 'a'] == null) return false;
+            ws = ws.children[c - 'a'];
+        }
+        return ws.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode ws = root;
+        for(int i = 0; i < prefix.length(); i++){
+            char c = prefix.charAt(i);
+            if(ws.children[c - 'a'] == null) return false;
+            ws = ws.children[c - 'a'];
+        }
+        return true;
+    }
+
+}
+
+class TrieNode {
+    public char val;
+    public boolean isWord;
+    public TrieNode[] children = new TrieNode[26];
+    public TrieNode() {}
+    TrieNode(char c){
+        TrieNode node = new TrieNode();
+        node.val = c;
+    }
+}
