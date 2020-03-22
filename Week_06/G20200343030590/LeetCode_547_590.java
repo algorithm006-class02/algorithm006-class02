5c3c72860eaf0c2f14460fa90d14b28b1d95dde6
luoqi <luoqi@bytedance.com>
2020-03-22 21:36:30 +0800

    590第六周作业

---
+++ Week_06/G20200343030590/LeetCode_547_590.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LeetCode_547_590 {
+
+    public int findCircleNum(int[][] M) {
+        int[] visited = new int[M.length];
+        int count = 0;
+        Queue< Integer > queue = new LinkedList< >();
+        for (int i = 0; i < M.length; i++) {
+            if (visited[i] == 0) {
+                queue.add(i);
+                while (!queue.isEmpty()) {
+                    int s = queue.remove();
+                    visited[s] = 1;
+                    for (int j = 0; j < M.length; j++) {
+                        if (M[s][j] == 1 && visited[j] == 0)
+                            queue.add(j);
+                    }
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
