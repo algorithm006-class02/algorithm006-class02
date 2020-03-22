af506e377550de294c384371764290a687c4fd2a
wanghaoxing <wanghaoxing123@gmail.com>
2020-03-22 22:35:29 +0800

    homework

---
+++ Week_06/G20200343030560/LeetCode_70_560.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    public class LeetCode_70_560
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            int[] dp = new int[n + 1];
+            dp[1] = 1;
+            dp[2] = 2;
+            for (int i = 3; i <= n; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[n];
+        }
+    }
+}
