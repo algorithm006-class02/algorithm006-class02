e078ed8c1952fd6c5d455c5db6e7533b2f13d0c6
张博 <zhangbo02@finupgroup.com>
2020-03-28 22:56:05 +0800

    第七周作业

---
+++ Week_07/G20200343030378/LeetCode_1122_378.java
@@ -0,0 +1,20 @@
+class LeetCode_1122_378{
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] bucket = new int[1001];
+        for(int num:arr1){
+            bucket[num]++;
+        }
+        int i = 0;
+        for(int num:arr2){
+            while(bucket[num]-- > 0){
+                arr1[i++] = num;
+            }
+        }
+        for(int j = 0; j < 1001; ++j){
+            while(bucket[j]-- > 0){
+                arr1[i++] = j;
+            }
+        }
+        return arr1;
+    }
+}
