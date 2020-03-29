415ce7ed7c7032fae2a374f67a0221f407c1d141
wanghaoxing <wanghaoxing123@gmail.com>
2020-03-29 22:18:30 +0800

    homework

---
+++ Week_07/G20200343030560/LeetCode_191_560.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    public class LeetCode_191_560
+    {
+        public int HammingWeight(uint n)
+        {
+            int bits = 0;
+            int mask = 1;
+            for (int i = 0; i < 32; i++)
+            {
+                if ((n & mask) != 0)
+                {
+                    bits++;
+                }
+                mask <<= 1;
+            }
+            return bits;
+        }
+    }
+}
