c98c101d3fad6b0512fab43fb55d483f40ca174f
algocosmos <algocosmos@126.com>
2020-03-29 19:16:25 +0800

    提交第 7 周作业

---
+++ Week_07/G20200343030578/LeetCode_56_578.java
@@ -0,0 +1,53 @@
+//给出一个区间的集合，请合并所有重叠的区间。
+//
+// 示例 1:
+//
+// 输入: [[1,3],[2,6],[8,10],[15,18]]
+//输出: [[1,6],[8,10],[15,18]]
+//解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+//
+//
+// 示例 2:
+//
+// 输入: [[1,4],[4,5]]
+//输出: [[1,5]]
+//解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
+// Related Topics 排序 数组
+
+
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution56 {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> res = new ArrayList<>();
+        if (intervals == null || intervals.length == 0) return res.toArray(new int[0][]);
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        int i = 0;
+        while (i < intervals.length) {
+            int left = intervals[i][0];
+            int right = intervals[i][1];
+            while (i < intervals.length - 1 && intervals[i + 1][0] <= right) {
+                i++;
+                right = Math.max(right, intervals[i][1]);
+            }
+            res.add(new int[]{left, right});
+            i++;
+        }
+        return res.toArray(new int[0][]);// 写法
+
+//        作者：powcai
+//        链接：https://leetcode-cn.com/problems/merge-intervals/solution/pai-xu-by-powcai/
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = new int[][]{{2, 6}, {15, 18}, {8, 10}, {1, 3}};
+
+        Solution56 sln = new Solution56();
+
+        sln.merge(intervals);
+    }
+}
