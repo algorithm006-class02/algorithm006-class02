78ca82539bb74c499924627c5b5c806380f45642
DawangC <chenchenghxt@vip.qq.com>
2020-03-30 02:47:05 +0800

    remove unnecessary code

--- Week_07/G20200343030372/Merge_intervals_56.java
+++ Week_07/G20200343030372/Merge_intervals_56.java
@@ -1,17 +1,3 @@
-/*
- * Copyright (C) 2014-2020 Nuhtech Technology(Beijing) Co.,Ltd.
- *
- * All right reserved.
- *
- * This software is the confidential and proprietary
- * information of Nuhtech Company of China.
- * ("Confidential Information"). You shall not disclose
- * such Confidential Information and shall use it only
- * in accordance with the terms of the contract agreement
- * you entered into with Nuhtech inc.
- *
- */
-
 package G20200343030372;
 
 import java.util.ArrayList;
