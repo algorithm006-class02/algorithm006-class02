7cc20949d69713cafc0833babe181fc1ca2defc1
loveindazhou <60743100+loveindazhou@users.noreply.github.com>
2020-03-29 17:59:30 +0800

    Create LeetCode_1244_368.java
    368-week 07

---
+++ Week_07/G20200343030368/LeetCode_1244_368.java
@@ -0,0 +1,27 @@
+class Leaderboard {
+
+    private TreeMap<Integer, Integer> map;
+
+    public Leaderboard() {
+        map = new TreeMap<>();
+    }
+
+    public void addScore(int playerId, int score) {
+        map.put(playerId, map.getOrDefault(playerId, 0) + score);
+    }
+
+    public int top(int K) {
+        int ans = 0;
+        List<Integer> list = new LinkedList<>(map.values());
+        Collections.sort(list, (n1, n2) -> n2 - n1 > 0 ? 1 : n2 == n1 ? 0 : -1);
+        for (int i = 0; i < K; i++) {
+            ans += list.get(i);
+        }
+        return ans;
+    }
+
+    public void reset(int playerId) {
+        map.remove(playerId);
+    }
+
+}
