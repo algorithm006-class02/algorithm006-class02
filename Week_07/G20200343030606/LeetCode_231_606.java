7e1cdecd4b80da545d3d16ee15883b98687076f4
陈靓 <appleidofcl@163.com>
2020-04-05 14:08:33 +0800

    week08-606

---
+++ Week_07/G20200343030606/LeetCode_231_606.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=231 lang=java
+ *
+ * [231] 2的幂
+ *
+ * https://leetcode-cn.com/problems/power-of-two/description/
+ *
+ * algorithms
+ * Easy (47.43%)
+ * Likes:    176
+ * Dislikes: 0
+ * Total Accepted:    53K
+ * Total Submissions: 110.7K
+ * Testcase Example:  '1'
+ *
+ * 给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 1
+ * 输出: true
+ * 解释: 2^0 = 1
+ * 
+ * 示例 2:
+ * 
+ * 输入: 16
+ * 输出: true
+ * 解释: 2^4 = 16
+ * 
+ * 示例 3:
+ * 
+ * 输入: 218
+ * 输出: false
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0) return false;
+        while (n % 2 == 0) n /= 2;
+        return n == 1;
+    }
+}
+// @lc code=end
+
