f01e4b0f36dc4ab8a02f0fa3d934aa1a03122b8c
wanghaoxing <wanghaoxing123@gmail.com>
2020-04-05 22:30:27 +0800

    homework

---
+++ Week_08/G20200343030560/LeetCode_541_560.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    class LeetCide_541_560
+    {
+        public string ReverseStr(string s, int k)
+        {
+            char[] a = s.ToCharArray();
+            for (int start = 0; start < a.Length; start += 2 * k)
+            {
+                int i = start, j = Math.Min(start + k - 1, a.Length - 1);
+                while (i < j)
+                {
+                    char tmp = a[i];
+                    a[i++] = a[j];
+                    a[j--] = tmp;
+                }
+            }
+            return new String(a);
+
+        }
+    }
+}
